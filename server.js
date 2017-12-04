75ea7f252ec127aaf42c7f0817adb1357bfafaa8
Jiri Spac <capajj@gmail.com>
2017-12-04 22:37:02 +0100

    added prettier and updated deps

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+require('dotenv').config()
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+const { PORT } = process.env
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: false,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(PORT, 'localhost', function(err, result) {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
 
-  console.log('Listening at localhost:3000');
-});
+  console.log(`Listening at http://localhost:${PORT}/`)
+})
